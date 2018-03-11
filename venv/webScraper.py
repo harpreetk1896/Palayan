53178db399b4db98997058d477574b3ec3859d94
harpreet <harpreetk1896@gmail.com>
2018-03-12 03:59:08 +0530

    first Commit

---
+++ venv/webScraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+file=open("Major_cities.txt","r")
+file2 = open("Traffic.txt", "w")
+for line in file:
+    line=line.rstrip()
+    page=requests.get("https://www.numbeo.com/traffic/in/"+line)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    for el in soup.find_all(class_="table_indices"):
+     k=el
+    var = list(k.children)[3]
+    var2 = list(var.children)[2]
+    val= var2.get_text()
+    val=val.lstrip()
+    file2.write(line+" "+val+"\n")
